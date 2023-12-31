2f8cfc68986ca73651fbb8f0f506ac2c44165379
briz1 <my1queen8my1king@gmail.com>
2023-12-31 19:01:19 +0700

    Two tasks completed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,37 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Задача 1 и 2");//Объявите три массива и выведите их значения в консоль
+    int [] arr = new int[] {1, 2, 3};
+        for (int value = 0; value < arr.length; value++) {
+            if (value == arr.length - 1) {
+                System.out.println(arr[value]);
+                break;
+            }
+            System.out.print(arr[value] + ", ");
+        }
+    double [] zzzz = {1.574, 7.654, 9.986};
+        for (int v = 0; v < zzzz.length; v++) {
+            if (v == zzzz.length - 1) {
+                System.out.println(zzzz[v]);
+                break;
+            }
+            System.out.print(zzzz[v] + ", ");
+        }
+
+    boolean[] upAndDown = {true, false, false, true, false, false };
+        for (int t=0; t<upAndDown.length; t++) {
+            if (t == upAndDown.length - 1) {
+                System.out.println(upAndDown[t]);
+                break;
+            }
+            System.out.print(upAndDown[t] + ", ");
+        }
+
+
+
+
+
     }
 }
